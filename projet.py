e8a830668699d4242dad59b599d014f6f0363449
thaliestjs <thaliestjs@gmail.com>
2022-02-14 17:05:49 -0500

    started working on main

--- projet.py
+++ projet.py
@@ -1,6 +1,21 @@
+import assign_infos
+import cleaning_pairs
+import lemmatizer
+#import clean_data
+
+# 1. Establish, clean and verify names list :
+# 		cleaning_pairs
+# 2. Access and clean corpus :
+# 		clean_data.py
+# 3. Lemmatize all the words except the ones listed (in step 1) :
+# 		lemmatizer.py et assign_infos
+
+
+
 def main():
-	pass
+	print("toast")
 
 
 
-
+if __name__ == '__main__':
+    main()
