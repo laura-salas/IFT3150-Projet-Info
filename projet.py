fc505af90239f709e6e865ad3c8fe7fc4bddbe5c
thaliestjs <thaliestjs@gmail.com>
2022-02-16 17:20:28 -0500

    menage + main

--- projet.py
+++ projet.py
@@ -1,19 +1,35 @@
+import os
+
 import assign_infos
 import cleaning_pairs
 import lemmatizer
-#import clean_data
+import clean_data
 
+# PLAN
 # 1. Establish, clean and verify names list :
-# 		cleaning_pairs
+# 		cleaning_pairs (not in this code ) then assign_infos
+# 		mais cleaning_pairs
 # 2. Access and clean corpus :
 # 		clean_data.py
 # 3. Lemmatize all the words except the ones listed (in step 1) :
-# 		lemmatizer.py et assign_infos
+# 		lemmatizer.py
+#
 
 
 
 def main():
+
 	print("toast")
+
+	# 1. assign_infos :
+	if not os.path.isdir("\REPERE\CLEANED\words_assigned.txt"):
+		assign_infos.main()
+
+	# 2. clean data
+	if len(os.listdir('/home/varun/temp') ) == 0:
+		clean_data.main()
+
+	lemmatize.main()
 
 
 
